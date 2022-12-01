1d4d1d36d4c80e1cce1b0fbdf3c37f854bfdaff2
zu3x <zu3x@fresnou.org>
2022-12-01 11:30:30 -0800

    third commit

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+  const {sequelize} = require("../models/index");
+  const {QueryTypes} = require("sequelize");
+
+  let pokemonItems = await sequelize.query('select * from pokemon', {type:QueryTypes.SELECT});
+  res.render('index', {pokemonItems})
 });
 
 module.exports = router;
